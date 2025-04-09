4baecb72939111eb0a254196714c850f5b0edea0
yuripaixao1005 <yuripaixao1005@gmail.com>
2025-04-08 21:55:18 -0300

    conflito resolvido

--- src/main/java/com/example/demo/repository/ClienteRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {}
--- src/main/java/com/example/demo/repository/StatusRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Status;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StatusRepository extends JpaRepository<Status, Long> {}
